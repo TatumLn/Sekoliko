9588cca28dc5910d9c362dd24c32aa7a483c19a3
TatumLn <linautatum501@gmail.com>
2023-04-14 12:32:16 +0300

    Sekoliko Premier Partie OK!

--- front-end/src/components/UserNavBar.vue
+++ front-end/src/components/UserNavBar.vue
@@ -3,7 +3,7 @@
   <nav class="navbar navbar-expand-lg navbar-light bg-light flex-column">
       <!--Titre-->
     <a class="navbar-brand" href="#">Sekoliko</a>
-    <!--Logo-->
+    <!--Logo sekoliko-->
     <img src="../assets/sekoliko.png" alt="" height="50">
     <!--Bouton responsive-->
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
